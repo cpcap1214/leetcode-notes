5cb95d80ad076e07ea8b4e91742b7d019d381469
Justin Chung <hi1214justin@gmail.com>
2025-10-25 00:12:14 +0800

    寫完 deploy 的流程

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://cpcap1214.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/leetcode-notes/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cpcap1214', // Usually your GitHub org/user name.
+  projectName: 'leetcode-notes', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
